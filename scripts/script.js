e89beb85e2ee4a77d77411ac85d98627e23b1a2c
Ediley Gomes <ediley.gomes@pucpr.edu.br>
2025-08-31 16:38:57 -0400

    ...

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
     
-    $("").mouseenter(function () { 
-        
-    });
+    $(".card-projeto").on({
+        mouseenter: function (e) {
+            $(this).find('.conteudo-card').css({
+                opacity: "1",
+                top: "-110px",
+                transition: "0.5s"
+            });
+        },
+        mouseleave: function (e) {
+            $(this).find('.conteudo-card').css({
+                opacity: "0",
+                top: "-70px",
+                transition: " opacity 0.5s ease-out, top 0.5s ease-out"
+            });
+        }
+    })
+
 
 });
